db8a24ab2d3258deeffe143b2ff89b9538fa15fe
Alex Kagarov <alexkagarov@gmail.com>
2020-08-10 10:05:22 +0300

    Initial Commit

---
+++ CharOccurence/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CharOccurence
+//
+//  Created by Mac on 10.08.2020.
+//  Copyright © 2020 hialex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
